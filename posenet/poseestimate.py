ff6ff204b01a923a98578fc34905b1efdbe5bfa2
TommyTo <haithienld@gmail.com>
2020-06-10 17:26:43 +0900

    update pose estimate

--- posenet/poseestimate.py
+++ posenet/poseestimate.py
@@ -1,3 +1,4 @@
+
 # Copyright 2019 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,71 +13,50 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""A demo which runs object detection on camera frames using GStreamer.
-
-Run default object detection:
-python3 detect.py
-
-Choose different camera and input encoding
-python3 detect.py --videosrc /dev/video1 --videofmt jpeg
-
-TEST_DATA=../all_models
-Run face detection model:
-python3 detect.py \
-  --model ${TEST_DATA}/mobilenet_ssd_v2_face_quant_postprocess_edgetpu.tflite
-
-Run coco model:
-python3 detect.py \
-  --model ${TEST_DATA}/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite \
-  --labels ${TEST_DATA}/coco_labels.txt
+"""A demo that runs hand tracking and object detection on camera frames using OpenCV. 2 EDGETPU
 """
 import argparse
 import collections
 import common
-import gstreamer
+import cv2
 import numpy as np
 import os
+from PIL import Image
 import re
-import svgwrite
-import time
+from edgetpu.detection.engine import DetectionEngine
+from pose_engine import PoseEngine
+import tflite_runtime.interpreter as tflite
 
 Object = collections.namedtuple('Object', ['id', 'score', 'bbox'])
 
+#==============================
+EDGES = (
+    ('nose', 'left eye'),
+    ('nose', 'right eye'),
+    ('nose', 'left ear'),
+    ('nose', 'right ear'),
+    ('left ear', 'left eye'),
+    ('right ear', 'right eye'),
+    ('left eye', 'right eye'),
+    ('left shoulder', 'right shoulder'),
+    ('left shoulder', 'left elbow'),
+    ('left shoulder', 'left hip'),
+    ('right shoulder', 'right elbow'),
+    ('right shoulder', 'right hip'),
+    ('left elbow', 'left wrist'),
+    ('right elbow', 'right wrist'),
+    ('left hip', 'right hip'),
+    ('left hip', 'left knee'),
+    ('right hip', 'right knee'),
+    ('left knee', 'left ankle'),
+    ('right knee', 'right ankle'),
+)
+#==============================
 def load_labels(path):
     p = re.compile(r'\s*(\d+)(.+)')
     with open(path, 'r', encoding='utf-8') as f:
        lines = (p.match(line).groups() for line in f.readlines())
        return {int(num): text.strip() for num, text in lines}
-
-def shadow_text(dwg, x, y, text, font_size=20):
-    dwg.add(dwg.text(text, insert=(x+1, y+1), fill='black', font_size=font_size))
-    dwg.add(dwg.text(text, insert=(x, y), fill='white', font_size=font_size))
-
-def generate_svg(src_size, inference_size, inference_box, objs, labels, text_lines):
-    dwg = svgwrite.Drawing('', size=src_size)
-    src_w, src_h = src_size
-    inf_w, inf_h = inference_size
-    box_x, box_y, box_w, box_h = inference_box
-    scale_x, scale_y = src_w / box_w, src_h / box_h
-
-    for y, line in enumerate(text_lines, start=1):
-        shadow_text(dwg, 10, y*20, line)
-    for obj in objs:
-        x0, y0, x1, y1 = list(obj.bbox)
-        # Relative coordinates.
-        x, y, w, h = x0, y0, x1 - x0, y1 - y0
-        # Absolute coordinates, input tensor space.
-        x, y, w, h = int(x * inf_w), int(y * inf_h), int(w * inf_w), int(h * inf_h)
-        # Subtract boxing offset.
-        x, y = x - box_x, y - box_y
-        # Scale to source coordinate space.
-        x, y, w, h = x * scale_x, y * scale_y, w * scale_x, h * scale_y
-        percent = int(100 * obj.score)
-        label = '{}% {}'.format(percent, labels.get(obj.id, obj.id))
-        shadow_text(dwg, x, y - 5, label)
-        dwg.add(dwg.rect(insert=(x,y), size=(w, h),
-                        fill='none', stroke='red', stroke_width='2'))
-    return dwg.tostring()
 
 class BBox(collections.namedtuple('BBox', ['xmin', 'ymin', 'xmax', 'ymax'])):
     """Bounding box.
@@ -88,70 +68,136 @@ class BBox(collections.namedtuple('BBox', ['xmin', 'ymin', 'xmax', 'ymax'])):
 def get_output(interpreter, score_threshold, top_k, image_scale=1.0):
     """Returns list of detected objects."""
     boxes = common.output_tensor(interpreter, 0)
-    category_ids = common.output_tensor(interpreter, 1)
+    class_ids = common.output_tensor(interpreter, 1)
     scores = common.output_tensor(interpreter, 2)
+    count = int(common.output_tensor(interpreter, 3))
 
     def make(i):
         ymin, xmin, ymax, xmax = boxes[i]
         return Object(
-            id=int(category_ids[i]),
+            id=int(class_ids[i]),
             score=scores[i],
             bbox=BBox(xmin=np.maximum(0.0, xmin),
                       ymin=np.maximum(0.0, ymin),
                       xmax=np.minimum(1.0, xmax),
                       ymax=np.minimum(1.0, ymax)))
+
     return [make(i) for i in range(top_k) if scores[i] >= score_threshold]
 
 def main():
     default_model_dir = '../all_models'
-    default_model = 'mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite'
-    default_labels = 'coco_labels.txt'
+    default_model = 'hand_tflite_graph_edgetpu.tflite'
+    default_labels = 'hand_label.txt'
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', help='.tflite model path',
                         default=os.path.join(default_model_dir,default_model))
     parser.add_argument('--labels', help='label file path',
                         default=os.path.join(default_model_dir, default_labels))
-    parser.add_argument('--top_k', type=int, default=3,
+    parser.add_argument('--top_k', type=int, default=1,
                         help='number of categories with highest score to display')
-    parser.add_argument('--threshold', type=float, default=0.1,
+    parser.add_argument('--camera_idx', type=str, help='Index of which video source to use. ', default = 0)
+    parser.add_argument('--threshold', type=float, default=0.5,
                         help='classifier score threshold')
-    parser.add_argument('--videosrc', help='Which video source to use. ',
-                        default='/dev/video0')
-    parser.add_argument('--videofmt', help='Input video format.',
-                        default='raw',
-                        choices=['raw', 'h264', 'jpeg'])
     args = parser.parse_args()
 
-    print('Loading {} with {} labels.'.format(args.model, args.labels))
-    interpreter = common.make_interpreter(args.model)
-    interpreter.allocate_tensors()
-    labels = load_labels(args.labels)
+    print('Loading Handtracking model {} with {} labels.'.format(args.model, args.labels))
+    #engine = DetectionEngine(args.model)
+    #labels = load_labels(args.labels)
+    engine = PoseEngine('models/mobilenet/posenet_mobilenet_v1_075_481_641_quant_decoder_edgetpu.tflite')
 
-    w, h, _ = common.input_image_size(interpreter)
-    inference_size = (w, h)
-    # Average fps over last 30 frames.
-    fps_counter  = common.avg_fps_counter(30)
+    # for detection
+    print('Loading Detection model {} with {} labels.'.format('../all_models/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite', '../all_models/coco_labels.txt'))
+    #interpreter2 = common.make_interpreter('../all_models/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite')
+    #interpreter2.allocate_tensors()
+    engine2 = DetectionEngine('../all_models/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite')
+    labels2 = load_labels('../all_models/coco_labels.txt')
 
-    def user_callback(input_tensor, src_size, inference_box):
-      nonlocal fps_counter
-      start_time = time.monotonic()
-      common.set_input(interpreter, input_tensor)
-      interpreter.invoke()
-      # For larger input image sizes, use the edgetpu.classification.engine for better performance
-      objs = get_output(interpreter, args.threshold, args.top_k)
-      end_time = time.monotonic()
-      text_lines = [
-          'Inference: {:.2f} ms'.format((end_time - start_time) * 1000),
-          'FPS: {} fps'.format(round(next(fps_counter))),
-      ]
-      print(' '.join(text_lines))
-      return generate_svg(src_size, inference_size, inference_box, objs, labels, text_lines)
+    cap = cv2.VideoCapture(args.camera_idx)
 
-    result = gstreamer.run_pipeline(user_callback,
-                                    src_size=(640, 480),
-                                    appsink_size=inference_size,
-                                    videosrc=args.videosrc,
-                                    videofmt=args.videofmt)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        cv2_im = frame
 
+        cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
+        pil_im = Image.fromarray(cv2_im_rgb)
+
+        #common.set_input(interpreter, pil_im)
+        #interpreter.invoke()
+        #objs = get_output(interpreter, score_threshold=args.threshold, top_k=args.top_k)
+        '''
+        objs = engine.detect_with_image(pil_im,
+                                  threshold=0.3,
+                                  keep_aspect_ratio=True,
+                                  relative_coord=True,
+                                  top_k=1)
+        '''
+        poses, inference_time = engine.DetectPosesInImage(np.uint8(pil_im.resize((641, 481), Image.NEAREST)))
+
+        print('Inference time: %.fms' % inference_time)
+        for pose in poses:
+            if pose.score < 0.4: continue
+            print('\nPose Score: ', pose.score)
+            for label, keypoint in pose.keypoints.items():
+                print(' %-20s x=%-4d y=%-4d score=%.1f' %
+                    (label, keypoint.yx[1], keypoint.yx[0], keypoint.score))
+
+        #cv2_im = append_objs_to_img(cv2_im, objs, labels)
+
+        # detection
+        #common.set_input(interpreter2, pil_im)
+        #interpreter2.invoke()
+        #objs = get_output(interpreter2, score_threshold=0.2, top_k=3)
+        objs = engine2.detect_with_image(pil_im,
+                                  threshold=0.2,
+                                  keep_aspect_ratio=True,
+                                  relative_coord=True,
+                                  top_k=3)
+        cv2_im = append_objs_to_img(cv2_im, objs, labels2)
+
+        cv2.imshow('frame', cv2_im)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+def append_objs_to_img(cv2_im, objs, labels):
+    height, width, channels = cv2_im.shape
+    for obj in objs:
+        x0, y0, x1, y1 = obj.bounding_box.flatten().tolist() #list(obj.bbox)
+        x0, y0, x1, y1 = int(x0*width), int(y0*height), int(x1*width), int(y1*height)
+        percent = int(100 * obj.score)
+        label = '{}% {}'.format(percent, labels.get(obj.label_id, obj.label_id))
+
+        cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 255, 0), 2)
+        cv2_im = cv2.putText(cv2_im, label, (x0, y0+30),
+                             cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
+    return cv2_im
+'''
+def draw_pose(dwg, pose, src_size, inference_box, color='yellow', threshold=0.2):
+    dwg = svgwrite.Drawing('', size=src_size)
+    src_size = (640, 480)
+    inference_box=
+    box_x, box_y, box_w, box_h = inference_box
+    scale_x, scale_y = src_size[0] / box_w, src_size[1] / box_h
+    xys = {}
+    for label, keypoint in pose.keypoints.items():
+        if keypoint.score < threshold: continue
+        # Offset and scale to source coordinate space.
+        kp_y = int((keypoint.yx[0] - box_y) * scale_y)
+        kp_x = int((keypoint.yx[1] - box_x) * scale_x)
+
+        xys[label] = (kp_x, kp_y)
+        dwg.add(dwg.circle(center=(int(kp_x), int(kp_y)), r=5,
+                           fill='cyan', fill_opacity=keypoint.score, stroke=color))
+
+    for a, b in EDGES:
+        if a not in xys or b not in xys: continue
+        ax, ay = xys[a]
+        bx, by = xys[b]
+        dwg.add(dwg.line(start=(ax, ay), end=(bx, by), stroke=color, stroke_width=2))
+'''
 if __name__ == '__main__':
     main()
