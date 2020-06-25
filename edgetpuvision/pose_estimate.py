8a97c8a89d02857847012c2fc699ca4d631cce5f
haithienld <haithienld@gmail.com>
2020-06-26 01:53:41 +0900

    update

--- edgetpuvision/pose_estimate.py
+++ edgetpuvision/pose_estimate.py
@@ -251,7 +251,6 @@ def render_gen(args):
 def add_render_gen_args(parser):
     default_model_dir = 'all_models'
     default_model = '/posenet_mobilenet_v1_075_481_641_quant_decoder_edgetpu.tflite'
-    parser = argparse.ArgumentParser()
     parser.add_argument('--model', help='.tflite model path',
                         default=os.path.join(default_model_dir,default_model))
     parser.add_argument('--labels', help='label file path',
